2120c0705ea61b8f28c26a24f733276099330f08
Farcool17 <farcool17@gmail.com>
2024-03-05 21:54:39 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -12,8 +12,23 @@ public class Payment {
     @Setter
     private String status;
     private Map<String, String> paymentData;
-    public Payment(String id, String method, String status, Map<String, String> paymentData){
 
+    public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        String[] methodsList = {"voucherCode", "cashOnDelivery"};
+        if (Arrays.stream(methodsList).noneMatch(item -> (item.equals(method)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.method = method;
+        }
+
+        String[] statusList = {"SUCCESS", "REJECTED"};
+        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+
+        this.paymentData = paymentData;
     }
-
 }
