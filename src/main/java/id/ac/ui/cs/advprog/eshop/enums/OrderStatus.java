0545b1bea653ab30f853808dec6fd7e0803c733a
Farcool17 <farcool17@gmail.com>
2024-03-05 08:05:29 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/OrderStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/OrderStatus.java
@@ -14,7 +14,7 @@ public enum OrderStatus {
         this.value = value;
     }
 
-    public static boolean containes(String param){
+    public static boolean contains(String param){
         for (OrderStatus orderStatus : OrderStatus.values()){
             if (orderStatus.name().equals(param)) {
                 return true;
